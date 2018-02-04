bbb0070f2f357726f6b28ef944b22f14b14d03ea
Arvernien <ahlopez.gtt@gmail.com>
2018-02-04 18:53:52 +0100

    j

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    #path('', views.IndexView.as_view(), name='index'),
+
+    path('', auth_views.login, {'template_name': 'polls/login.html', }, name='login'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('finca/', views.FincaView.as_view(), name='finca'),
     path('crea/', views.CrearFinca.as_view(), name='crea'),
 ]
+
