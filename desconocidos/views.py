27b5fbe4c07344eb58269bb9713b70f860846c38
Disc <arvernienhl@gmail.com>
2018-02-20 18:29:14 +0100

    creacion desconocidos

--- desconocidos/views.py
+++ desconocidos/views.py
@@ -6,6 +6,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def Desconocidos(request):
-    lista_desc = Desconocido.objects.all().order_by(((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())[:100]
-    context = {'lista_desc': lista_desc, }
+    if request.method == "POST" and request.POST.get('criterio') != '':
+        texto = request.POST.get('criterio')
+        lista_desc = Desconocido.objects.filter(refcat__icontains=texto).order_by(((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())[:100]
+        context = {'lista_desc': lista_desc, }
+    else:
+        lista_desc = Desconocido.objects.all().order_by(
+            ((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())[:100]
+        context = {'lista_desc': lista_desc, }
     return render(request, 'desconocidos/desconocidos.html', context)
