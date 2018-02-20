79e306a0f7ff45973c9073dd4470555f8c19a50e
Arvernien <ahlopez.gtt@gmail.com>
2018-02-20 22:30:47 +0100

    lista desc

--- desconocidos/views.py
+++ desconocidos/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render
 from .models import Desconocido
-from django.db.models import F
+from django.db.models import F, Q
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 @login_required
 def Desconocidos(request):
-    if request.method == "POST" and request.POST.get('criterio') != '':
-        texto = request.POST.get('criterio')
-        lista_desc = Desconocido.objects.filter(refcat__icontains=texto).order_by(((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())[:100]
-        context = {'lista_desc': lista_desc, }
+    q = request.GET.get('q')
+    if q:
+        texto = request.GET.get('q')
+        desc = Desconocido.objects.filter(Q(refcat__icontains=q) |
+                                                Q(fk_muni__nombre__icontains=q)
+                                                ).order_by(((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())
     else:
-        lista_desc = Desconocido.objects.all().order_by(
-            ((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())[:100]
-        context = {'lista_desc': lista_desc, }
+        desc = Desconocido.objects.all().order_by(
+            ((F('b_liquidable') / 100) * F('fk_muni__tipo_impositivo') / 100).desc())
+    limite_pagina = 50
+    paginador = Paginator(desc, limite_pagina)
+    pag = request.GET.get('pag')
+    lista_desc = paginador.get_page(pag)
+    num_paginas = lista_desc.paginator.num_pages
+    if not pag:
+        pag = 1
+    pagina_actual = int(pag)
+    total = len(desc)
+    inicio = pagina_actual * limite_pagina + 1 - 50
+    final = pagina_actual * limite_pagina
+    if final > total:
+        final = total
+
+    if num_paginas <= 11 or pagina_actual <= 6:
+        paginas = [x for x in range(1, min(num_paginas + 1, 12))]
+    elif pagina_actual > num_paginas - 6:
+        paginas = [x for x in range(num_paginas - 10, num_paginas + 1)]
+    else:
+        paginas = [x for x in range(pagina_actual - 5, pagina_actual + 6)]
+    context = {'lista_desc': lista_desc,
+               'paginas': paginas,
+               'inicio': inicio,
+               'final': final,
+               'total': total
+               }
     return render(request, 'desconocidos/desconocidos.html', context)
